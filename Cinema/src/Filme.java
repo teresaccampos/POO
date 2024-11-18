046911fb7ce58306ee4bca8a8ab9a314437ed664
Paulo Henrique <contactph51@gmail.com>
2024-11-18 14:09:29 -0300

    remove properties unused and fix typo in method cadastrarFilme

--- Cinema/src/Filme.java
+++ Cinema/src/Filme.java
@@ -7,11 +7,9 @@ import java.util.ArrayList;
 
 public class Filme {
   private int id;
-  private int classificacao;
-  private int ano;
-  private int genero;
   private String titulo;
-  private String sinopse;
+  private int classificacao;
+  private int genero;
   private String status;
 
   //Método construtor
@@ -19,10 +17,8 @@ public class Filme {
   public Filme(int id, int classificacao, int ano, int genero, String titulo, String sinopse, String status) {
     this.id = id;
     this.classificacao = classificacao;
-    this.ano = ano;
     this.genero = genero;
     this.titulo = titulo;
-    this.sinopse = sinopse;
     this.status = "A";
   }
 
@@ -42,10 +38,6 @@ public class Filme {
     return classificacao;
   }
 
-  public int getAno() {
-    return ano;
-  }
-
   public Genero getGenero(ArrayList<Genero> generos) {
     Genero gen = null;
     for (Genero g : generos) {
@@ -61,10 +53,6 @@ public class Filme {
     return titulo;
   }
 
-  public String getSinopse() {
-    return sinopse;
-  }
-
   public String getStatus() {
     return status;
   }
@@ -73,23 +61,15 @@ public class Filme {
     this.classificacao = classificacao;
   }
 
-  public void setAno(int ano) {
-    this.ano = ano;
-  }
-
   public void setTitulo(String titulo) {
     this.titulo = titulo;
-  }
-
-  public void setSinopse(String sinopse) {
-    this.sinopse = sinopse;
   }
 
   public void setStatus(String status) {
     this.status = status;
   }
 
-  public static boolean inserirFilme(Filme filme) throws IOException {
+  public static boolean cadastrarFilme(Filme filme) throws IOException {
     boolean retorno = true;
     BufferedWriter bw = new BufferedWriter(new FileWriter("filme.txt", true));
      
@@ -100,25 +80,33 @@ public class Filme {
       return retorno;
     } 
 
-  public static ArrayList<Filme> listarFilmes(ArrayList<Genero> generos) throws IOException {
-    ArrayList<Filme> filmes = new ArrayList<>();
-    BufferedReader br = new BufferedReader(new FileReader("filme.txt"));
-    
-    String linha;
-
-    while((linha = br.readLine()) != null) {
-      String[] dados = linha.split(";");
-      int id = Integer.parseInt(dados[0]);
-      int idGenero = Integer.parseInt(dados[1]);
-      String titulo = dados[2];
-
-      Filme filme = new Filme(id, idGenero, titulo );
-      filmes.add(filme);
+  public static boolean editarFilme(int idFilme, String novoTitulo, ArrayList<Genero> generos) throws IOException {
+      boolean retorno = false;
+  
+      ArrayList<Filme> filmes = listarFilmes(generos);
+      
+      try (BufferedWriter bw = new BufferedWriter(new FileWriter("filme.txt"))) {
+        for (Filme f : filmes) {
+          if (f.getId() == idFilme) {
+            retorno = true;
+            Filme filme = new Filme(f.getId(), f.genero, novoTitulo);
+            bw.write(filme.getId() + ";" + filme.genero + ";" + filme.getTitulo());
+          } else {
+            bw.write(f.getId() + ";" + f.genero + ";" + f.getTitulo());
+          }
+          bw.newLine();
+        }
+      } catch (IOException e) {
+        retorno = false;
+        e.printStackTrace();
+      }
+  
+      if (!retorno) {
+        System.out.println("Gênero não encontrado.");
+      }
+  
+      return retorno;
     }
-    
-    br.close();
-    return filmes;
-  }
 
   public static Filme consultarFilme(String nomeFilme) {
     Filme filme = null;
@@ -142,31 +130,23 @@ public class Filme {
     return filme;
   }
 
-  public static boolean editarFilme(int idFilme, String novoTitulo, ArrayList<Genero> generos) throws IOException {
-    boolean retorno = false;
-
-    ArrayList<Filme> filmes = listarFilmes(generos);
+  public static ArrayList<Filme> listarFilmes(ArrayList<Genero> generos) throws IOException {
+    ArrayList<Filme> filmes = new ArrayList<>();
+    BufferedReader br = new BufferedReader(new FileReader("filme.txt"));
     
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter("filme.txt"))) {
-      for (Filme f : filmes) {
-        if (f.getId() == idFilme) {
-          retorno = true;
-          Filme filme = new Filme(f.getId(), f.genero, novoTitulo);
-          bw.write(filme.getId() + ";" + filme.genero + ";" + filme.getTitulo());
-        } else {
-          bw.write(f.getId() + ";" + f.genero + ";" + f.getTitulo());
-        }
-        bw.newLine();
-      }
-    } catch (IOException e) {
-      retorno = false;
-      e.printStackTrace();
-    }
+    String linha;
 
-    if (!retorno) {
-      System.out.println("Gênero não encontrado.");
-    }
+    while((linha = br.readLine()) != null) {
+      String[] dados = linha.split(";");
+      int id = Integer.parseInt(dados[0]);
+      int idGenero = Integer.parseInt(dados[1]);
+      String titulo = dados[2];
 
-    return retorno;
+      Filme filme = new Filme(id, idGenero, titulo );
+      filmes.add(filme);
+    }
+    
+    br.close();
+    return filmes;
   }
 }
